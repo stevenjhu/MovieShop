ba888de754e4f26bc1b2479a9b6c3e0685b88268
stevenjhu <s19990922@gmail.com>
2022-08-22 16:53:26 -0400

    Add project files.

---
+++ MovieShopMVC/Controllers/AccountController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieShopMVC/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            //go to db and get the movie info by movid id
+            //and send the data(Model) to the view
+            //ADO.NET
+            //Dapper StackOverflow -> Micro ORM
+            //Entity Framework Core => Full ORM for Microsoft
+
+            //Select * from Movies where id = 12;
+            //Code is Maintenable, Reusable, Readable, Extensible, Testable
+            //layers => Layered architecture
+            //Onion, Clean Architecture
+            
+
+            return View();
+        }
+    }
+}
