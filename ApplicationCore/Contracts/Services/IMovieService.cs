fa0ecd9522c7578d8a856aeffec7cacb2425f747
stevenjhu <s19990922@gmail.com>
2022-08-24 10:46:53 -0400

    Added interface implementation

--- ApplicationCore/Contracts/Services/IMovieService.cs
+++ ApplicationCore/Contracts/Services/IMovieService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.Contracts.Services
 {
-    public interface Interface1
+    public interface IMovieService
     {
     }
 }
--- Infrastructure/Services/MovieService.cs
+++ Infrastructure/Services/MovieService.cs
@@ -1,26 +1,14 @@
-﻿using System;
+﻿using ApplicationCore.Contracts.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ApplicationCore.Contracts.Services;
 
 namespace Infrastructure.Services
 {
     public class MovieService : IMovieService
     {
-        public List<MovieCardModel> GetTop30GrossingMovies()
-        {
-            var movieRepository = new MovieRepository();
-            var movies = movieRepository.GetTop30GrossingMovies().Take(6);
-
-            var movieCards = new List<MovieCardModel>();
-            foreach (var movie in movies)
-            {
-                movieCards.Add(new MovieCardModel { Id = movie.Id, Title = movie.Title, PosterUrl = movie.PosterUrl });
-            }
-
-            return movieCards;
-        }
+        
     }
 }
