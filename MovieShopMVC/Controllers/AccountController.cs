e1e2ad7a62b26b6377ed758c8496d403ee5f8d91
stevenjhu <s19990922@gmail.com>
2022-08-29 16:48:02 -0400

    Created Login and Register View

--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginModel model)
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            //showing empty register view
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterModel model, string FIRSTNAME, string firstname, string xyz)
         {
             return View();
         }
